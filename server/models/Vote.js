57a1830e9d245613ce69e1ad3ddbf6fd05fcf627
Adam556678 <mahmudramadan75@yahoo.com>
2025-10-05 18:15:49 +0300

    Make a Vote model

---
+++ server/models/Vote.js
@@ -0,0 +1,8 @@
+const mongoose = require("mongoose");
+
+const VoteSchema = new mongoose.Schema({
+    userId : {type: mongoose.Schema.Types.ObjectId, ref: "User", required: true},
+    targetId: {type: mongoose.Schema.Types.ObjectId, required: true},
+    targetType: {type: String, enum: ["Post", "Comment"], required: true},
+    voteType: {type: String, enum: ["up", "down"], required: true}
+}, {timestamps: true});
