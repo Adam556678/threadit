d3e1c3f355dfdefc2d4445eb9c68f5bdcbe0e99b
Adam556678 <mahmudramadan75@yahoo.com>
2025-10-06 15:11:34 +0300

    Fix comment issues

--- server/routes/post.js
+++ server/routes/post.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const mongoose = require("mongoose");
 const auth = require("../middlewares/auth.js");
 const postMiddleware = require("../middlewares/member_post.js");
+const Comment = require("../models/Comment.js");
 
 // Get post content - GET
 router.get("/:postId", auth, postMiddleware, async (req, res) => {
@@ -24,5 +25,35 @@ router.get("/:postId/comments", auth, postMiddleware, async (req, res) => {
         return res.status(500).json({message: "Something went wrong"});
     }
 });
+
+/*
+Add comment route - POST
+params:
+    - postId
+body:
+    - text
+*/
+router.post("/:postId/add-comment", auth, postMiddleware, async (req, res) => {
+    try {
+        const {postId} = req.params;
+        const {text} = req.body;
+
+        // create comment document and push it to comments list
+        const comment = await Comment.create({
+            author: req.userId,
+            postId,
+            text
+        })
+        req.post.comments.push(comment._id);
+        await req.post.save();
+
+        return res.status(200).json({message: "Comment added"});
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({message: "Something went wrong"});
+    }
+
+});
+
 
 module.exports = router;
